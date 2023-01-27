4b8d05aad19cc7cd5e0572995db50883a86d37cd
ainoal <77739788+ainoal@users.noreply.github.com>
2023-01-27 16:01:50 +0200

    2.2 errors fixed

--- Exercise2/ex_2_2.jl
+++ Exercise2/ex_2_2.jl
@@ -2,17 +2,19 @@ using Unitful.DefaultSymbols: mm, µm, m
 using Unitful
 
 function main() 
-    (o_x, o_y) = (320µm, 240µm)     # Optical center of the camera
-    (s_x, s_y) = (10µm, 10µm)       # Effective size of pixel
-    f = 16mm                        # Focal length
+    o_x =  320          # Optical center of the camera
+    o_y = 240
+    s_x = 10*10^-6      # Effective size of pixel
+    s_y = 10*10^-6
+    f = 16*10^-3        # Focal length
 
     K = [-f/s_x 0 o_x;
         0 -f/s_y o_y;
         0 0 1]
 
-    transf = K * (1; 0; 8)          # Transformation
-    p = (transf(1)/transf(3), transf(2)/transf(3))  # Divide by z
-    println("The pixel coordinates are p = (", p(1), "," p(2), ").")
+    transf = K * [1; 0; 8]          # Transformation
+    p = (transf[1]/transf[3], transf[2]/transf[3])  # Divide by z
+    println("The pixel coordinates are p = (", p[1], ", ", p[2], ").")
 
     return nothing
 end
