ff5f701249d78a566d83d893f809027903ccd862
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-06 12:11:21 +0200

    ex 3.2ab

--- Exercise3/src/ex_3_2.jl
+++ Exercise3/src/ex_3_2.jl
@@ -1,7 +1,11 @@
+# NOTE TO SELF: CHECK THE VARIANCE OF THE GAUSSIAN NOICE
+
 using Images
 using Plots
 using Noise
 using Random
+using ImageFiltering
+using Statistics
 
 function main()
     # Load and plot the image
@@ -11,9 +15,43 @@ function main()
     #p = plot_image(lena)
 
     # Add zero-mean gaussian noise with variance 0.01 and display the image
+    # CHECK THE VARIANCE
     noise_variance = 0.01
     gaussian_noise_lena = add_gauss(lena, noise_variance, clip=true)
-    p = plot_image(gaussian_noise_lena)
+    p = plot_image(gaussian_noise_lena, title = "Gaussian noise")
+
+    # Exercise part a: linear filtering with a Gaussian kernel
+    # i. σ = 0.5
+    sigma_i = 0.5
+    img_gaussian_i = imfilter(gaussian_noise_lena, Kernel.gaussian(sigma_i))
+    p_gaussian_i = plot_image(img_gaussian_i; title = "Gaussian, σ = $sigma_i")
+    # ii. σ = 1
+    sigma_ii = 1
+    img_gaussian_ii = imfilter(gaussian_noise_lena, Kernel.gaussian(sigma_ii))
+    p_gaussian_ii = plot_image(img_gaussian_ii; title = "Gaussian, σ = $sigma_ii")
+    # iii. σ = 2
+    sigma_iii = 2
+    img_gaussian_iii = imfilter(gaussian_noise_lena, Kernel.gaussian(sigma_iii))
+    p_gaussian_iii = plot_image(img_gaussian_iii; title = "Gaussian, σ = $sigma_iii")
+
+    # Exercise part b: Median filtering
+    # i. filter size 3 x 3
+    window_i = (3, 3)
+    img_median_i = mapwindow(median, gaussian_noise_lena, window_i)
+    p_median_i = plot_image(img_median_i; title = "Median, $(join(window_i, " × ")) window")
+    # ii. filter size 5 x 5
+    window_ii = (5, 5)
+    img_median_ii = mapwindow(median, gaussian_noise_lena, window_ii)
+    p_median_ii = plot_image(img_median_ii; title = "Median, $(join(window_ii, " × ")) window")
+    # iii. filter size 9 x 9
+    window_iii = (9, 9)
+    img_median_iii = mapwindow(median, gaussian_noise_lena, window_iii)
+    p_median_iii = plot_image(img_median_iii; title = "Median, $(join(window_iii, " × ")) window")
+
+    p = plot(p, p_gaussian_i, p_gaussian_ii, p_gaussian_iii, p_median_i, 
+        p_median_ii, p_median_iii; size=(700, 1050), layout=@layout [x x; x x; x x; x ])
 end
+
+
 
 main()
