c1ea52ac14b6e46c496b6f82cd11448c724615a3
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-15 13:45:31 +0200

    ex5.2 idea

--- Exercise5/src/ex_5_2.jl
+++ Exercise5/src/ex_5_2.jl
@@ -1,6 +1,23 @@
+# Line fitting with gradient method
 
+using MAT
 
 function main()
+    data = matread(joinpath(@__DIR__, "../data/xy.mat"))
+
+    # Gradient descent
+    starting_point = (1, 1, 1)
+    stepsize = 1*10^-8
+    iterations = 1000
+
+    # Code the iteration: 
+    for i in 1:iterations
+        # Calculate params+1 according to the formula in the exercise.
+    end 
+    # Use the last params from the iteration to define the function of the line.
+
+    # Plot the data points and the line in the same plot.
+
 end
 
 main()
