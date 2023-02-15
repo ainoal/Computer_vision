41951de01615bb09b0ddf40d149ad08c039c7246
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-15 12:43:28 +0200

    5.3 list of points along the edges

--- Exercise5/src/ex_5_3.jl
+++ Exercise5/src/ex_5_3.jl
@@ -6,9 +6,15 @@ using ImageEdgeDetection: Percentile
 function main()
     # Exercise part a: Load the image and perform edge detection.
     img = load(joinpath(@__DIR__, "../data/circle.png"))
+    edge_points = edge_detection(img)
+
+
+end
+
+function edge_detection(img)
     dimensions = size(img)
 
-    # Find gradients G_x and G_y
+    # Find gradients G_x and G_y.
     Gx, Gy = imgradients(img, KernelFactors.sobel)
     
     Gx_normalized = normalize(Gx)
@@ -21,7 +27,7 @@ function main()
 
     binary_img = falses(reverse(dimensions))
 
-    # For each pixel
+    # For each pixel, update binary image to represent the edges.
     for i in 1:dimensions[1]
         for j in 1:dimensions[2]
             if ((t1 < sqrt(grayx[i, j] ^ 2 + grayy[i, j] ^ 2)) || t2 > sqrt(grayx[i, j] ^ 2 + grayy[i, j] ^ 2))
@@ -32,10 +38,20 @@ function main()
         end
     end
 
+    # Thin the edges.
     binary = thinning(binary_img)
     edges = plot_image(Gray.(binary), title = "Edges")
 
-
+    # Make a list of the pixels along the edge.
+    list = []
+    for i in 1:dimensions[1]
+        for j in 1:dimensions[2]
+            if (binary[i, j] == 1)
+                push!(list, (i, j))
+            end
+        end
+    end
+    return list
 end
 
 plot_image(img; kws...) = 
