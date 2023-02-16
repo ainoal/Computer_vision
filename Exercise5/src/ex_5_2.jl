a400ce1f81323facf6c19e28b4b21ff30d33ca5c
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-17 00:54:36 +0200

    ex5.2 plotting not done yet

--- Exercise5/src/ex_5_2.jl
+++ Exercise5/src/ex_5_2.jl
@@ -1,36 +1,58 @@
 # Line fitting with gradient method
 
 using MAT
-using Flux
+using LinearAlgebra
+#using Flux
 #using ForwardDiff
 #using ReverseDiff
 
 function main()
     data = matread(joinpath(@__DIR__, "../data/xy.mat"))
+    points = data["xy"] 
     #size = size(data)
     #println(size)
     size = 512
+    println(sizeof(points))
 
     # Gradient descent
-    starting_point = (1, 1, 1)
+    starting_point = [1, 1, 1]
     stepsize = 1*10^-8
     iterations = 1000
 
     #params = []
     #push!(params, starting_point)
     prev_params = starting_point
+    params = starting_point
 
     # Code the iteration: 
-    for i in 1:iterations
-        gradient = 2
-        params = prev_params - stepsize * gradient
+
+   # println(points[2, 1])
+    for j in 1:iterations
+        params = prev_params - stepsize * partial_derivative(prev_params, points)
         prev_params = params
-    end 
+    end
+
+    #println(params[3])
     # Use the last params from the iteration to define the function of the line.
-    # f = params[1] * x + params[2] * y + params[3]
+    #f(x, y) = params[1] * x + params[2] * y + params[3]
+
+    # This part not working
+    #x = range(0, 512, length=512)
+    #y = ((-params[1] / params[2]) * x + (- params[3] / params[2]))
 
     # Plot the data points and the line in the same plot.
+    #plot(x, y)
 
+end
+
+function partial_derivative(prev_params, points)
+    gradient = [0.0, 0.0, 0.0]
+    for i in 1:3
+        gradient[1] += 2 * (prev_params[1]*points[1, i] + prev_params[2]*points[2, i] + prev_params[3]) * points[1, i]
+        gradient[2] += 2 * (prev_params[1]*points[1, i] + prev_params[2]*points[2, i] + prev_params[3]) * points[2, i]
+        gradient[3] += 2 * (prev_params[1]*points[1, i] + prev_params[2]*points[2, i] + prev_params[3])
+    end
+    return normalize(gradient)
 end
 
 #=function partial_derivative(datapoint, size)
--- Exercise5/src/ex_5_3.jl
+++ Exercise5/src/ex_5_3.jl
@@ -11,16 +11,18 @@ function main()
     edge_points = edge_detection(img)
 
     # WHAT IS THE BEAUTIFUL WAY TO IMPLEMENT THIS?:
-    edgepoints_size = 161
+    println(size(edge_points))
+    edgepoints_size = 158
     #x = size(getfield.(edge_points, 2))
     #println(x)
+    #a = edge_points[1][6]
 
     # Exercise part b: Implement SVD
     X = zeros(edgepoints_size, 4)
     #println(edge_points[1][1])
     # Construct matrix X that represents the circle.
     for i in 1:edgepoints_size
-        X[i, 1] = (edge_points[i][1])^2 + (edge_points[i][2]^2)
+        X[i, 1] = (edge_points[i][1])^2 + (edge_points[i][2])^2
         X[i, 2] = edge_points[i][1]
         X[i, 3] = edge_points[i][2]
         X[i, 4] = 1
@@ -28,11 +30,26 @@ function main()
 
     # Use SVD for solving the resulting set of linear equations.
     svd_vals = svd(X)
-    
+    V = svd_vals.V
+    #println(V)
+
+    # Plot the circle found on the original image.
+    #plot(V[:, 4])
+    a = V[1, 4]
+    b = V[2, 4]
+    c = V[3, 4]
+    d = V[4, 4]
+    f(x, y) = a * (x^2 + y^2) + b*x + c*y + d
+
+    gr()
+    #=contour(
+        f
+    )=#
 end
 
 function edge_detection(img)
     dimensions = size(img)
+    #println(dimensions)
 
     # Find gradients G_x and G_y.
     Gx, Gy = imgradients(img, KernelFactors.sobel)
@@ -42,15 +59,14 @@ function edge_detection(img)
     grayx = Gx_normalized .|> Gray
     grayy = Gy_normalized .|> Gray
 
-    t1 = 0.75
-    t2 = 0.70
+    t = 0.01
 
     binary_img = falses(reverse(dimensions))
 
     # For each pixel, update binary image to represent the edges.
     for i in 1:dimensions[1]
         for j in 1:dimensions[2]
-            if ((t1 < sqrt(grayx[i, j] ^ 2 + grayy[i, j] ^ 2)) || t2 > sqrt(grayx[i, j] ^ 2 + grayy[i, j] ^ 2))
+            if (t < sqrt(grayx[i, j] ^ 2 + grayy[i, j] ^ 2))
                 binary_img[i, j] = true
             else
                 binary_img[i, j] = false
@@ -69,6 +85,7 @@ function edge_detection(img)
         for j in 1:dimensions[2]
             if (binary[i, j] == 1)
                 push!(list, (i, j))
+                #println(".")
             end
         end
     end
