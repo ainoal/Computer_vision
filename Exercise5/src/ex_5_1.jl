e676fc8137fced7d3e286b13ae6b5f7b43387c0f
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-16 20:25:36 +0200

    5.1 almost working

--- Exercise5/src/ex_5_1.jl
+++ Exercise5/src/ex_5_1.jl
@@ -1,9 +1,11 @@
 using Images
 using ImageFiltering
+using Plots
 
 function main()
     I = load(joinpath(@__DIR__, "../data/lena_bw.png"))
     imgsize = size(I)
+    p = plot(I)
 
     # Exercise part a: Construct stack of Gaussians.
     gaussians = [I, I, I, I, I]     # Initialize the array
@@ -23,79 +25,27 @@ function main()
     # Exercise part b: Find local extrema in D.
     # Local features can be found by finding local extrema in the stack of DoG.
     window = (3, 3, 3)
-    #DoG_all_levels = [D[1], D[2], D[3], D[4]]
-    # use function cat instead
-    DoG_all_levels = cat(D[1], D[2], D[3], D[4], dims=3)
+    DoG_all_levels = cat(D[1], D[2], D[3], D[4]; dims=3)
+    extrema = mapwindow(find_extrema, DoG_all_levels, window)
     
-
-    #extrema_all_levels = []
-    #b = mapwindow(extrema, D[2], window)
-    # = getfield.(b, 2)
-    #println(c)
-
-    
-
     # Exercise part c: Limit the number of local features with tresholding.
     t = 0.03
-    for n in 1:4
-        #min = getfield.(extrema_all_levels[n], 1)
-        #max = getfield.(extrema_all_levels[n], 2)
-
-        #min_channels = channelview(min)
-        #max_channels = channelview(max)
-        #println(size(min_channels))
-        
+    for level in 1:4
+        channels = channelview(extrema[level])
+        features = abs.(channels[1, :, :]) .> t
+        feature_points = Gray.(features)
+        p = plot!(feature_points)
     end
-    #extrema_all_levels[4, 2]
+    display(p)
 end
 
-function find_maxima(vals)
+function find_extrema(vals)
     c = centered(vals)[0, 0]
-    if (c = maximum(vals))
-        ret = c
+    if ((c == maximum(vals)) || (c == minimum(vals)))
+        return c
     else
-        ret = zero(c)
+        return zero(c)
     end
-    return ret
 end
 
 main()
-
-    #=for k in 1:4
-        minmax = mapwindow(extrema, D[k], window)
-        # TODO: Find minmax of the higher and lower level as well and update
-        # minmax according to that
-        if (k == 1)
-            minmax_higher = mapwindow(extrema, D[k+1], window)
-            for l in 1:imgsize[1]
-                for m in 1:imgsize[2]
-                    if (minmax_higher[l, m] > minmax[l, m])
-                        minmax[l, m] = minmax_higher[l, m]
-                    end
-                end
-            end
-        elseif (k == 4)
-            minmax_lower = mapwindow(extrema, D[k-1], window)
-            for l in 1:imgsize[1]
-                for m in 1:imgsize[2]
-                    if (minmax_lower[l, m] > minmax[l, m])
-                        minmax[l, m] = minmax_lower[l, m]
-                    end
-                end
-            end
-        else
-            minmax_lower = mapwindow(extrema, D[k-1], window)
-            minmax_higher = mapwindow(extrema, D[k+1], window)
-            for l in 1:imgsize[1]
-                for m in 1:imgsize[2]
-                    if (minmax_higher[l, m] > minmax[l, m])
-                        minmax[l, m] = minmax_higher[l, m]
-                    end
-                    if (minmax_lower[l, m] > minmax[l, m])
-                        minmax[l, m] = minmax_lower[l, m]
-                    end
-                end
-            end
-        end
-        push!(extrema_all_levels, minmax)
-    end=#
--- Exercise5/src/ex_5_2.jl
+++ Exercise5/src/ex_5_2.jl
@@ -16,28 +16,28 @@ function main()
     stepsize = 1*10^-8
     iterations = 1000
 
-    params = []
-    push!(params, starting_point)
+    #params = []
+    #push!(params, starting_point)
     prev_params = starting_point
-
 
     # Code the iteration: 
     for i in 1:iterations
-        # Calculate params+1 according to the formula in the exercise.
-        #f(a, b, c) = (a * data[i])
-        #next_params = prev_params - stepsize * grad
+        gradient = 2
+        params = prev_params - stepsize * gradient
+        prev_params = params
     end 
     # Use the last params from the iteration to define the function of the line.
+    # f = params[1] * x + params[2] * y + params[3]
 
     # Plot the data points and the line in the same plot.
 
 end
 
-function partial_derivative(datapoint, size)
+#=function partial_derivative(datapoint, size)
     for i in 1:size
         f(a, b, c) = (a * datapoint[1] + b * datapoint[2] + c)^2
         df_a = gradient(f, a)
     end
-end
+end=#
 
 main()
