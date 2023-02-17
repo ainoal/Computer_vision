c385c76bd40959f89e04581f9eff006578d67a1f
ainoal <77739788+ainoal@users.noreply.github.com>
2023-02-17 16:27:09 +0200

    5.2 cleaned up

--- Exercise5/src/ex_5_2.jl
+++ Exercise5/src/ex_5_2.jl
@@ -2,9 +2,6 @@
 
 using MAT
 using LinearAlgebra
-#using Flux
-#using ForwardDiff
-#using ReverseDiff
 
 function main()
     data = matread(joinpath(@__DIR__, "../data/xy.mat"))
@@ -12,27 +9,18 @@ function main()
 
     size = 512
 
-    #print(points[:, 4])
-
-
     p = plot((points[1, 1], points[2, 1]), seriestype=:scatter)
     #for i in 1:512
     for i in 1:100
         p = plot!((points[1, i], points[2, i]), seriestype=:scatter)
 
     end
-    #display(p)
-
-
-    #println(points_matrix)
 
     # Gradient descent
     starting_point = [1, 1, 1]
     stepsize = 1*10^-8
     iterations = 1000
 
-    #params = []
-    #push!(params, starting_point)
     prev_params = starting_point
     params = starting_point
 
@@ -41,22 +29,11 @@ function main()
         params = prev_params - stepsize * partial_derivative(prev_params, points)
         prev_params = params
     end
-    #println(partial_derivative(prev_params, points))
-    #println(params)
-    # Use the last params from the iteration to define the function of the line.
-    #f(x, y) = params[1] * x + params[2] * y + params[3]
 
-    # This part not working
+    # Use the last params from the iteration to define the function of the line.
     x = range(0, 100, length=512)
     y = ((-params[1] / params[2]) * x .+ (- params[3] / params[2]))
     plot!(x, y)
-
-    #plot()
-
-    # Plot the data points and the line in the same plot.
-    #plot(points, seriestype=:scatter)
-    #plot!(x, y)
-    #display(p)
 
 end
 
@@ -67,15 +44,8 @@ function partial_derivative(prev_params, points)
         gradient[2] += 2 * (prev_params[1]*points[1, i] + prev_params[2]*points[2, i] + prev_params[3]) * points[2, i]
         gradient[3] += 2 * (prev_params[1]*points[1, i] + prev_params[2]*points[2, i] + prev_params[3])
     end
-    #return gradient
-    return normalize(gradient)
+    return gradient
+    #return normalize(gradient)
 end
-
-#=function partial_derivative(datapoint, size)
-    for i in 1:size
-        f(a, b, c) = (a * datapoint[1] + b * datapoint[2] + c)^2
-        df_a = gradient(f, a)
-    end
-end=#
 
 main()
