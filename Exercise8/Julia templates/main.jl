e1cb4300e7b1919d0736e0037b23924ab396f8a1
ainoal <77739788+ainoal@users.noreply.github.com>
2023-03-26 14:28:54 +0300

    8.1 find fundamental matrix

--- Exercise8/Julia templates/main.jl
+++ Exercise8/Julia templates/main.jl
@@ -13,13 +13,13 @@ plot_image!(p, I; kws...) = plot!(p, I; aspect_ratio=:equal, size=size(I), frame
 
 
 # Load images
-Il = load("../../data/books1.jpg")
-Ir = load("../../data/books2.jpg")
+Il = load(joinpath(@__DIR__, "../data/books1.jpg"))
+Ir = load(joinpath(@__DIR__, "../data/books2.jpg"))
 
 
-pl = [249.1886  251.1865  185.7675  186.6054   96.5758   19.0369   87.8358  201.7579 72.6772
+pl = [249.1886  251.1865  185.7675  186.6054   96.5758   19.0369   87.8358  201.7579 72.6772;
 204.1058   50.1468   51.0201  126.5525   80.9284  148.0381  200.0909  205.3973 54.0182]
-pr = [154.4705  257.6632  199.4353  166.7630   95.2722   10.0000   35.7504  121.1513 85.2440
+pr = [154.4705  257.6632  199.4353  166.7630   95.2722   10.0000   35.7504  121.1513 85.2440;
 223.2215   70.5352   48.8615  124.8812   47.2440   86.0000  157.2300  205.4297 12.9543]
 
 
@@ -31,8 +31,8 @@ include("triangulation.jl")
 function task1(Il, Ir, pl, pr)
     # TODO: Implement find_fundamental_matrix, estimate_cameras, linear_triangulation from file triangulation.jl
     F = find_fundamental_matrix(pl, pr)
-    Ml, Mr = estimate_cameras(F)
-    X = linear_triangulation(pl, Ml, pr, Mr)
+    #Ml, Mr = estimate_cameras(F)
+    #X = linear_triangulation(pl, Ml, pr, Mr)
 
     # TODO: Calculate reprojection error using Ml, Mr and X
 end
@@ -72,5 +72,5 @@ function task3(Il, Ir, pl, pr)
 end
 
 task1(Il, Ir, pl, pr)
-task2(Il, Ir, pl, pr)
-task3(Il, Ir, pl, pr)
+#task2(Il, Ir, pl, pr)
+#task3(Il, Ir, pl, pr)
--- Exercise8/Julia templates/triangulation.jl
+++ Exercise8/Julia templates/triangulation.jl
@@ -3,24 +3,81 @@ include("utils.jl")
 
 function find_fundamental_matrix(pl, pr)
     # TODO Fill in your code for finding fundamental matrix F
+        # Transform to normalized image coordinates.
+        T_L = get_normalization_matrix(pl[:, 1:8], 8)
+        T_R = get_normalization_matrix(pr[:, 1:8], 8)
+    
+        left_homogeneous = vcat(pl[:, 1:8], ones(1, 8))
+        right_homogeneous = vcat(pr[:, 1:8], ones(1, 8))
+        left_normalized = T_L * left_homogeneous
+        right_normalized = T_R * right_homogeneous
+    
+        # Determine the fundamental matrix Fˆ from the singular vector corresponding to
+        # smallest singular value of Aˆ.
+        A_hat = get_matrix_A(left_normalized, right_normalized)
+        svd_A = svd(A_hat)
+        V = svd_A.V
+        F_hat = transpose(reshape(V[:, end], 3, 3))
+        svd_F = svd(F_hat)
+        
+        # Calculate Fˆ′ from Fˆ using SVD such that Fˆ′ = UD′V^T and D'
+        # has the smallest singular value set equal to zero.
+        D_corrected = Diagonal(svd_F.S)
+        F_normalized = svd_F.U * D_corrected * svd_F.Vt
+    
+        # Denormalize. See exercises week 6.
+        F = transpose(T_R) * F_normalized * T_L
+    
+        # Test with a ninth coordinate pair if F works.
+        ninth_coord_left = [249; 203; 1]
+        ninth_coord_right = [156; 222; 1]
+        test = transpose(ninth_coord_right) * F * ninth_coord_left
+        println(test)
     return F
+end
+
+function get_normalization_matrix(points2d, N)
+    # Constructing matrix T for normalization for image (2D) points.
+    sumx = 0
+    sumy = 0
+    sumd = 0
+
+    for i in 1:N
+        sumx += points2d[1, i]
+        sumy += points2d[2, i]
+    end
+    x = (1/N) * sumx
+    y = (1/N) * sumy
+
+    for i in 1:N
+        sumd += sqrt((points2d[1, i] - x)^2 + (points2d[2, i] - y)^2)
+    end
+    d = (1/N) * sumd
+
+    T = [sqrt(2)/d 0 -(sqrt(2))*x/d;
+        0 sqrt(2)/d -(sqrt(2))*y/d;
+        0 0 1]
+
+    return T
 end
 
 function find_epipoles(F)
     # TODO Fill in your code for finding epipoles el and er
-   return el, er
+   #return el, er
 end
 
 
 function estimate_cameras(F, er)
     # TODO Fill in your code for estimating cameras Ml and Mr from F
+    Ml = 0
+    Mr = 0
     return Ml, Mr
 end
 estimate_cameras(F) = estimate_cameras(F, find_epipoles(F)[2])
 
 function linear_triangulation(pl, Ml, pr, Mr)
     # TODO Fill in your code for linear triangulation
-    return X
+    #return X
 end
 
 
