fe04650ac4d0941d3f6b7420ff18a85cd319257e
ainoal <77739788+ainoal@users.noreply.github.com>
2023-03-30 12:03:58 +0300

    9.1 algorithm, treshold needs to be changed

--- Exercise9/src/ex_9_1.jl
+++ Exercise9/src/ex_9_1.jl
@@ -12,6 +12,33 @@ function main()
         img_matrix = cat(img_matrix, new_img; dims=3)
     end
 
+    # Convert to grayscale
+    gray_matrix = Gray.(img_matrix)
+
+    # Implement motion segmentation based on image difference.
+    treshold = 0.99
+    dimensions = size(gray_matrix)
+    difference_matrix = falses(dimensions[1], dimensions[2], dimensions[3] - 1)
+    for i in 1:dimensions[1]
+        for j in 1:dimensions[2]
+            for img in 1:(dimensions[3] - 1)
+                # Step 1: Compute a pointwise image difference
+                difference = gray_matrix[i, j, img] - gray_matrix[i, j, img + 1]
+                # Step 2: if the difference is larger than treshold, the pixel is moving
+                if (abs(difference) > treshold)
+                    difference_matrix[i, j, img] = true
+                end
+            end
+        end
+    end
+
+    for frame in 1:(dimensions[3] - 1)
+        motion = plot_image(Gray.(difference_matrix[:, :, frame]), title = "Difference")
+        display(motion)
+    end
 end
+
+plot_image(img; kws...) = 
+    plot(img; aspect_ratio=:equal, size=size(img), framestyle=:none, kws...)
 
 main()
