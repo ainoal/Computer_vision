0701a30ded280d3947195fa147e9cadd97107a26
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-05 14:02:51 +0300

    10.1 use template for similarity, 10.2 algorithm

--- Exercise10/src/ex_10_2.jl
+++ Exercise10/src/ex_10_2.jl
@@ -1,23 +1,14 @@
 using MAT
 using Plots
+using Images
 
 function main()
     slow_data = matread(joinpath(@__DIR__, "../data/slow_square.mat"))
     track(slow_data, "slow_square")
 
+
     fast_data = matread(joinpath(@__DIR__, "../data//fast_square.mat"))
     track(fast_data, "fast_square")
-
-    # The same tracking algorithm works for both image sequences.
-    # However, if the largest allowed displacement was smaller than
-    # the movement of the square between two frames, the algorithm
-    # would not work anymore. 
-
-    # With more complex images, choosing the correct maximum displacement 
-    # should be done more precisely because choosing a too large values
-    # could lead to matching the template with a wrong area of the picture.
-    # With these image sequences, however, there is no fear for that since
-    # there is only one white square in the otherwise black scene.
 end
 
 function track(data, seq_name)
@@ -26,16 +17,43 @@ function track(data, seq_name)
     seq = data[seq_name]
     no_of_images = size(seq)[3]
 
-    largest_allowed_displacement = 25
+    # Generate a half resolution sequence
+    halfres = zeros(Int64(size(seq)[1]/2), Int64(size(seq)[2]/2), no_of_images)
+    for i in 1:no_of_images
+        halfres[:, :, i] = imresize(seq[:, :, i], ratio = 1/2)
+    end
+
+    largest_allowed_displacement = 12
 
     p = start_point
     sz = size(template)
 
     for imgno in 1:no_of_images
         thisimg = seq[:, :, imgno]
+        thishalfres = halfres[:, :, imgno]
         largest_similarity = -Inf
         d = [0; 0]
 
+        # Perform tracking using the half resolution image
+        for dx in -largest_allowed_displacement:largest_allowed_displacement
+            for dy in -largest_allowed_displacement:largest_allowed_displacement
+                c = 0
+                for l in 1:(sz[1]/2)
+                    for m in 1:(sz[2]/2)
+                        template_coord = [Int64(floor(l)), Int64(floor(m))]
+                        thishalfres_coord = Vector{Int64}([floor(p[1]/2 + l + dx); floor(p[2]/2 + m + dy)])
+                        c += ssd(template_coord, thishalfres_coord, template, thishalfres)
+                    end
+                end
+                if (c > largest_similarity)
+                    largest_similarity = c
+                    d = [dx*2; dy*2]
+                end
+            end
+        end
+        p = p + d
+
+        # Perform tracking using the full resolution image
         for dx in -largest_allowed_displacement:largest_allowed_displacement
             for dy in -largest_allowed_displacement:largest_allowed_displacement
                 c = 0
@@ -47,8 +65,6 @@ function track(data, seq_name)
                     end
                 end
                 if (c > largest_similarity)
-                    #println(c)
-                    #println(d)
                     largest_similarity = c
                     d = [dx; dy]
                 end
