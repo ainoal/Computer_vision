f44b4d79fd25acfc60b66ea292f223740ba973ef
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-20 12:07:21 +0300

    12.2 functions for estimating location and calculating distance

--- Exercise12/src/ex_12_2.m
+++ Exercise12/src/ex_12_2.m
@@ -1,17 +1,25 @@
 load("data\localization.mat")
 
-A_red = double([projection_matrix(:, 1) projection_matrix(:, 2) ...
-    -[double(red_projected); 1] (double(z_coordinate) * projection_matrix(:, 3) + projection_matrix(:, 4))])
+red3d = double(red_3d)
+point_red = estimate_location(red_projected, projection_matrix, z_coordinate)
+distance_red = calc_distance(red3d, point_red)
 
-[~, ~, V_red] = svd(A_red)
-point_red = V_red(:, end)
 
-point_red_dehomogenized = double([point_red(1)/point_red(4); ...
-    point_red(2)/point_red(4); z_coordinate])
+function point_dehomogenized = estimate_location(projected_point, projection_matrix, z_coordinate)
+    A = double([projection_matrix(:, 1) projection_matrix(:, 2) -[double(projected_point); 1] ...
+        (double(z_coordinate) * projection_matrix(:, 3) + projection_matrix(:, 4))])
+    
+    [~, ~, V_red] = svd(A)
+    point = V_red(:, end)
+    
+    point_dehomogenized = double([point(1)/point(4); ...
+        point(2)/point(4); z_coordinate])
+end
 
 % Calculate the distance between the estimated 3D location
 % and the true location
-red3d = double(red_3d)
-distance = sqrt((red3d(1) - point_red_dehomogenized(1))^2 + ...
-    (red3d(2) - point_red_dehomogenized(2))^2 + ...
-    (red3d(3) - point_red_dehomogenized(3))^2)
+function distance = calc_distance(true, estimated)
+    distance = sqrt((true(1) - estimated(1))^2 + ...
+        (true(2) - estimated(2))^2 + (true(3) - estimated(3))^2)
+end
+
